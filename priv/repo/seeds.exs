5ec335d552ac36787934972ea23619a72513deef
Harrison <harrisontanh@gmail.com>
2023-07-11 17:38:00 -0700

    Show or hide streamed calendars on the client side based on their visibility field

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,14 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias LiveCal.Repo
+alias LiveCal.Scheduling
+
+[
+  %Scheduling.Calendar{name: "Holidays", description: "time for an adventure", visibility: :public},
+  %Scheduling.Calendar{name: "Birthdays", description: "remember to find gifts", visibility: :private},
+  %Scheduling.Calendar{name: "Meetups", description: "channeling inner social butterfly", visibility: :private},
+  %Scheduling.Calendar{name: "Sport Matches", description: "oggy oggy oggy, oi oi oi", visibility: :public}
+] |> Enum.map(&Repo.insert!/1)
+
